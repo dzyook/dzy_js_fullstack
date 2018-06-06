67b122535cf9625a11bca17599ba7cbd248a7958
dzyook <13767954532@163.com>
2018-06-06 11:42:47 +0800

    '球员页'

--- wxapp/nba/pages/player/player.js
+++ wxapp/nba/pages/player/player.js
@@ -7,66 +7,66 @@ Page({
   /**
    * 页面的初始数据
    */
-  
+
   data: {
     canshow: true,
     canshow2: true,
     playernum: 5,
-    playernum2:5,
-    player1:[],
-    player2:[],
-    player3:[],
-    player4:[],
-    player5:[]
+    playernum2: 5,
+    player1: [],
+    player2: [],
+    player3: [],
+    player4: [],
+    player5: []
   },
   showmore: function (e) {
     let canshow = this.data.canshow;
-    if(canshow){
+    if (canshow) {
       this.setData({
-      canshow:false,
-      playernum: 10,
-    })
-    }else{
+        canshow: false,
+        playernum: 10,
+      })
+    } else {
       this.setData({
-        canshow:true,
+        canshow: true,
         playernum: 5,
       })
-    } 
+    }
   },
   showmore2: function (e) {
     let canshow = this.data.canshow2;
-    if(canshow){
+    if (canshow) {
       this.setData({
-      canshow2:false,
-      playernum2: 10,
-    })
-    }else{
+        canshow2: false,
+        playernum2: 10,
+      })
+    } else {
       this.setData({
-        canshow2:true,
+        canshow2: true,
         playernum2: 5,
       })
-    } 
+    }
   },
   onShareAppMessage() {
     return {
       path: `/pages/index/index`,
-      success: function(res) {
+      success: function (res) {
       },
-      fail: function(res) {
+      fail: function (res) {
       }
     }
   },
 
   onLoad: function (options) {
-      wx.request({
-        url: 'https://www.easy-mock.com/mock/5b0cbb4e60480528d24b9b2b/nba/news#!method=get',
-        success:(res) =>{
-          this.setData({
-            player1:res.data.data.player1,
-            player2:res.data.data.player2
-          })
-        }
-      })
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5b0cbb4e60480528d24b9b2b/nba/news#!method=get',
+      success: (res) => {
+        this.setData({
+          player1: res.data.data.player1,
+          player2: res.data.data.player2
+        })
+      }
+    })
 
   },
 
@@ -91,31 +91,5 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
