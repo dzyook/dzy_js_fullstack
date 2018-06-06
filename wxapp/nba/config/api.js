71be31dca742396694d2d3e3288698f840a0769b
dzyook <13767954532@163.com>
2018-06-06 11:35:26 +0800

    'aoi'

--- wxapp/nba/config/api.js
+++ wxapp/nba/config/api.js
@@ -1,3 +1,1 @@
 export const API_BASE = 'https://www.easy-mock.com/mock/5b0cbb4e60480528d24b9b2b/nba/news#!method=get'
-
-export const API_CREATE = 'https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get'
