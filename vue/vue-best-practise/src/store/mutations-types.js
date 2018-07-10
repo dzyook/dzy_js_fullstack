599352e3627565a58091b7abf182ff93b1e43da7
dzyook <13767954532@163.com>
2018-07-10 23:29:13 +0800

    vuex

---
+++ vue/vue-best-practise/src/store/mutations-types.js
@@ -0,0 +1,8 @@
+// 新增和删除一条计划
+export const SAVE_PLAN = 'SAVE_PLAN'
+export const DELETE_PLAN = 'DELETE_PLAN'
+
+// 增加或减少总时间
+export const ADD_TOTAL_TIME = 'ADD_TOTAL_TIME'
+export const DEC_TOTAL_TIME = 'DEC_TOTAL_TIME'
+// totalTime
