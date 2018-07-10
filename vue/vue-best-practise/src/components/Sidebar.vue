c019c5091c02b94d3cf762aa0451be804b5815af
dzyook <13767954532@163.com>
2018-07-10 23:28:49 +0800

    vuex个人任务

---
+++ vue/vue-best-practise/src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h1 class="text-center">已有时长</h1>
+        </div>
+        <div class="panel-body">
+            <div class="text-center">{{ time }}小时</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Sidebar',
+  computed: {
+    time () {
+      return this.$store.state.totalTime
+    }
+  }
+}
+</script>
+<style>
+</style>
